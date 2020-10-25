27544facbfc3f6e81e4a5388470ead43abab84fc
Adonay Puszczynski <adonay.puszczynski@livetouch.com.br>
2020-10-25 10:58:54 -0300

    home

---
+++ AbraTeCodigo/Source/Controller/Cell/ExposicaoViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ExposicaoViewCell.swift
+//  AbraTeCodigo
+//
+//  Created by livetouch on 25/10/20.
+//
+
+import UIKit
+
+class ExposicaoViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ AbraTeCodigo/Source/Controller/Cell/InstituicaoViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  InstituicaoViewCell.swift
+//  AbraTeCodigo
+//
+//  Created by livetouch on 25/10/20.
+//
+
+import UIKit
+
+class InstituicaoViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
